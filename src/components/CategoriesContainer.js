d5ab56c95f548eff2f5ce8a06a460b03a783e754
github username <github email address>
2019-04-12 20:35:34 -0400

    add s to http

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -11,8 +11,8 @@ class CategoriesContainer extends Component {
   }
 
   componentDidMount = () => {
-    // fetch("http://localhost:3000/api/v1/categories")
-    fetch("http://hangry-fapoon.herokuapp.com/api/v1/categories")
+    // fetch("https://localhost:3000/api/v1/categories")
+    fetch("https://hangry-fapoon.herokuapp.com/api/v1/categories")
     .then(res => res.json())
     .then(categories => {
       this.setState({
