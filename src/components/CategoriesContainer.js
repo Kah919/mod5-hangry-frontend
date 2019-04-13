b46f8055c8b346605ead908965a4116d8851a6bf
github username <github email address>
2019-04-12 20:53:23 -0400

    add mode cors

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -12,7 +12,7 @@ class CategoriesContainer extends Component {
 
   componentDidMount = () => {
     // fetch("https://localhost:3000/api/v1/categories")
-    fetch("https://hangry-fapoon.herokuapp.com/api/v1/categories")
+    fetch("https://hangry-fapoon.herokuapp.com/api/v1/categories", { mode: 'no-cors' })
     .then(res => res.json())
     .then(categories => {
       this.setState({
